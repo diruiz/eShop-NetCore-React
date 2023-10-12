a159deb85a54129116543c1f4104f76f8147b803
Diego Ruiz <diegoandresruiz@gmail.com>
2023-10-12 11:12:21 -0500

    validator

---
+++ eShop.Backend/tests/eShop.UnitTests/Application/Validations/CreateCatalogCommandValidatorTests.cs
@@ -0,0 +1,75 @@
+﻿using AutoFixture;
+using eShop.Application.Commands;
+using eShop.Application.Validations;
+using eShop.Models.eShopDbModels;
+using FluentAssertions;
+using FluentValidation.TestHelper;
+
+namespace eShop.UnitTests.Application.Validations;
+
+public class CreateCatalogCommandValidatorTests
+{
+    private readonly CreateCatalogCommandValidator _validator;
+    private readonly IFixture _fixture;
+    public CreateCatalogCommandValidatorTests()
+    {
+        _validator = new CreateCatalogCommandValidator();
+        _fixture = new Fixture();
+    }
+
+    [Fact]
+    public void ShouldHaveErrorWhenNameIsNull()
+    {
+        // Arrange
+        var catalog = CreateCatalogObject();
+        catalog.Name = null;
+
+        // Act
+        var result = _validator.TestValidate(catalog);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.Name);
+    }
+
+    [Fact]
+    public void ShouldHaveErrorWhenNameIsEmpty()
+    {
+        // Arrange
+        var catalog = CreateCatalogObject();
+        catalog.Name = string.Empty;
+
+        // Act
+        var result = _validator.TestValidate(catalog);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.Name);
+    }
+
+    [Fact]
+    public void ShouldHaveErrorWhenPictureFileNameIsNotUrl()
+    {
+        // Arrange
+        var catalog = CreateCatalogObject();
+        catalog.PictureFileName = "random string";
+
+        // Act
+        var result = _validator.TestValidate(catalog);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.PictureFileName);
+    }
+
+    [Fact]
+    public void ShouldNotHaveError()
+    {
+        var catalog = CreateCatalogObject();
+        var result = _validator.TestValidate(catalog);
+        result.IsValid.Should().Be(true);
+    }
+    private CreateCatalogCommand CreateCatalogObject()
+    {
+        return _fixture.Build<CreateCatalogCommand>()
+            .With(x => x.PictureFileName, "https://image.com")            
+            .Create();
+    }
+}
