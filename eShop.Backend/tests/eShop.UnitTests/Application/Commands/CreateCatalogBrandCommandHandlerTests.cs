51d832391a1684eaf8f32abc741d7ba96226c5c5
Diego Ruiz <diegoandresruiz@gmail.com>
2023-10-11 21:38:01 -0500

    repository unit test with in memory db

---
+++ eShop.Backend/tests/eShop.UnitTests/Application/Commands/CreateCatalogBrandCommandHandlerTests.cs
@@ -0,0 +1,54 @@
+﻿using AutoFixture;
+using AutoMapper;
+using eShop.Application.Commands;
+using eShop.Application.Commands.Handlers;
+using eShop.Domain.Services.Interfaces;
+using eShop.Models.eShopDbModels;
+using Moq;
+
+namespace eShop.UnitTests.Domain.Commands;
+
+public sealed class CreateCatalogBrandCommandHandlerTests
+{
+    private readonly Mock<ICatalogBrandService> _service;
+    private readonly CreateCatalogBrandCommandHandler _handler;
+    private readonly IFixture _fixture;
+    private readonly Mock<IMapper> _mapper;
+
+    public CreateCatalogBrandCommandHandlerTests()
+    {
+        this._service = new Mock<ICatalogBrandService>();
+        this._mapper = new Mock<IMapper>();
+        this._handler = new CreateCatalogBrandCommandHandler(_service.Object, _mapper.Object);
+        this._fixture = new Fixture();
+
+    }
+
+    [Fact]
+    public async Task ShouldCreateSingleCatalogBrandItem()
+    { 
+        // Arrange
+        var command = CreateCatalogBrandCommandObject();
+        CancellationToken cancellationToken = default;
+        var response = CreateCatalogBrandObject();
+
+        _mapper.Setup(x => x.Map<CatalogBrand>(command)).Returns(response);
+        _service.Setup(x => x.CreateCatalogBrand(It.IsAny<CatalogBrand>())).Returns(Task.FromResult(response));
+
+        // Act
+        var result = await _handler.Handle(command, cancellationToken);
+
+        // Assert
+        result.Equals(response);
+    }
+
+    private CreateCatalogBrandCommand CreateCatalogBrandCommandObject()
+    {
+        return _fixture.Build<CreateCatalogBrandCommand>().Create();
+    }
+
+    private CatalogBrand CreateCatalogBrandObject()
+    {
+        return _fixture.Build<CatalogBrand>().Without(x => x.Catalogs).Create();
+    }
+}
---
+++ eShop.Backend/tests/eShop.UnitTests/Persistence/Repository/CatalogTypeRepositoryTests.cs
@@ -0,0 +1,47 @@
+﻿using AutoFixture;
+using eShop.Models.eShopDbModels;
+using eShop.Persistence.Context;
+using eShop.Persistence.Repository.Implementation;
+using Microsoft.EntityFrameworkCore;
+
+namespace eShop.UnitTests.Persistence.Repository;
+
+public sealed class CatalogTypeRepositoryTests
+{
+    private EShopDBContext _context;
+    private CatalogTypeRepository _repository;
+    private IFixture _fixture;
+    public CatalogTypeRepositoryTests()
+    {
+        _fixture = new Fixture();
+        var options = new DbContextOptionsBuilder<EShopDBContext>()
+            .UseInMemoryDatabase(databaseName: "DatabaseName")
+            .Options;
+
+        _context = new EShopDBContext(options);
+        _repository = new CatalogTypeRepository(_context);
+
+    }
+
+    [Fact]
+    public async Task TestYourMethod()
+    {
+        // Arrange
+        var catalogTypeObject = CreateCatalogTypeObject();             
+
+        // Act: 
+        await _repository.Add(catalogTypeObject);
+        _context.SaveChanges();
+
+        // Assert: 
+        Assert.Equal(1, catalogTypeObject.Id);
+    }
+
+    private CatalogType CreateCatalogTypeObject()
+    {
+        return _fixture.Build<CatalogType>()
+            .With(x => x.Id, 0)
+            .Without(x => x.Catalogs)            
+            .Create();
+    }
+}
