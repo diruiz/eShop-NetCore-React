05db421ddbf18421bcf438a432b8667314beed54
Diego Ruiz <diegoandresruiz@gmail.com>
2023-10-12 21:56:29 -0500

    consume service

--- eshop.frontend/src/components/Home.tsx
+++ eshop.frontend/src/components/Home.tsx
@@ -3,19 +3,22 @@ import { ICatalog } from '../models/catalog.model';
 import { getPaginatedCatalog } from '../services/catalog.service';
 
 export default function Home() {
+	const [page, setPage] = useState<number>(0);
+	const [limit, setLimit] = useState<number>(10);
 	const [catalog, setCatalog] = useState<ICatalog | undefined>(undefined);
 
 	useEffect(() => {
 		const getData = async () => {
-			const result = await getPaginatedCatalog(0,2);
-			console.log(result);
+			const result = await getPaginatedCatalog(page,limit);
+			setCatalog(result);
 		}
 		getData();		   
   },[]);
   
   return (
     <div>
-      <h1>Welcome to my app</h1>      
+      <h1>Welcome to my app</h1> 
+			{JSON.stringify(catalog)}     
     </div>
   );
 }
--- eshop.frontend/src/services/catalog.service.ts
+++ eshop.frontend/src/services/catalog.service.ts
@@ -1,12 +1,14 @@
+import { ICatalog } from "../models/catalog.model";
+
 export async function getPaginatedCatalog(page: number, limit: number)
 {
 	const url = `${process.env.REACT_APP_API_ENDPOINT}/v1/Catalog?page=${page}&limit=${limit}`;
 	console.log(url);
 	const response = await fetch(url);
-	if (response.status !== 201) {
+	if (!response.ok) {
 			throw new Error(response.statusText);
 	}
 
 	const result = await response.json();	
-	return result;
+	return result as ICatalog;
 }
