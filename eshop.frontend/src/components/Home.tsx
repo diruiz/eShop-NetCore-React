1435d611dab0403e68ec3089a23c3b1a4647992f
Diego Ruiz <diegoandresruiz@gmail.com>
2023-10-12 22:04:45 -0500

    pages number

--- eshop.frontend/src/components/Home.tsx
+++ eshop.frontend/src/components/Home.tsx
@@ -5,11 +5,13 @@ import { getPaginatedCatalog } from '../services/catalog.service';
 export default function Home() {
 	const [page, setPage] = useState<number>(0);
 	const [limit, setLimit] = useState<number>(10);
+	const [pagesNumber, setPagesNumber] = useState<number>(0);
 	const [catalog, setCatalog] = useState<ICatalog | undefined>(undefined);
 
 	useEffect(() => {
 		const getData = async () => {
 			const result = await getPaginatedCatalog(page,limit);
+			setPagesNumber(Math.ceil(result.count/limit));
 			setCatalog(result);
 		}
 		getData();		   
@@ -18,6 +20,7 @@ export default function Home() {
   return (
     <div>
       <h1>Welcome to my app</h1> 
+			{pagesNumber}
 			{JSON.stringify(catalog)}     
     </div>
   );
