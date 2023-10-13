fc14bd375e67350591ce52c52b0d65c8ad7f7c93
Diego Ruiz <diegoandresruiz@gmail.com>
2023-10-13 11:09:15 -0500

    assets

--- eshop.frontend/src/components/Card/Card.tsx
+++ eshop.frontend/src/components/Card/Card.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { ICatalogItem } from '../../models/catalog.model';
 import './Card.css';
+import addItem from '../../assets/images/add.svg'
 
 export default function Card({item} : {item : ICatalogItem}) {
   return (
 		<div className="esh-catalog-item">
 			<div className="esh-catalog-thumbnail-wrapper">
 				<div className="esh-catalog-thumbnail-icon d-flex justify-content-center">
-					<img className="esh-catalog-thumbnail-icon-svg" src="assets/images/add.svg" />
+					<img className="esh-catalog-thumbnail-icon-svg" src={addItem} />
 				</div>
-				<img className="esh-catalog-thumbnail" src={item.pictureFileName} />
+				<img className="esh-catalog-thumbnail" alt={item.name} src={item.pictureFileName} />
 			</div>
 			<div className="esh-catalog-details d-flex justify-content-between align-items-center">
 				<div className="esh-catalog-name ml-3">
