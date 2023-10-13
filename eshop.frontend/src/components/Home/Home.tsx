05eb0acf0b7570a7ca7fac59c2f7457a4a5a2195
Diego Ruiz <diegoandresruiz@gmail.com>
2023-10-13 11:31:49 -0500

    pagination

--- eshop.frontend/src/components/Home/Home.tsx
+++ eshop.frontend/src/components/Home/Home.tsx
@@ -3,21 +3,27 @@ import { ICatalog } from '../../models/catalog.model';
 import { getPaginatedCatalog } from '../../services/catalog.service';
 import './Home.css';
 import Card from '../Card/Card';
+import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
 
 export default function Home() {
-	const [page, setPage] = useState<number>(0);
+	const [currentPage, setCurrentPage] = useState<number>(0);
 	const [limit, setLimit] = useState<number>(8);
-	const [pagesNumber, setPagesNumber] = useState<number>(0);
+	const [pagesCount, setPagesCount] = useState<number>(0);
 	const [catalog, setCatalog] = useState<ICatalog | undefined>(undefined);
 
 	useEffect(() => {
 		const getData = async () => {
-			const result = await getPaginatedCatalog(page,limit);
-			setPagesNumber(Math.ceil(result.count/limit));
+			const result = await getPaginatedCatalog(currentPage, limit);
+			setPagesCount(Math.ceil(result.count/limit));
 			setCatalog(result);
 		}
 		getData();		   
-  },[page, limit]);
+  },[currentPage, limit]);
+
+	function handlePagination(event: any, newPage: number){
+		event.preventDefault();
+		setCurrentPage(newPage);
+	}
   
   return (
     <div>
@@ -32,6 +38,33 @@ export default function Home() {
 						);
 					})
 				}
+			</div>
+			<div>
+				<Pagination aria-label="Page navigation">
+					<PaginationItem disabled={currentPage <= 0}>
+					<PaginationLink
+                onClick={e => handlePagination(e, currentPage - 1)}
+                previous
+                href="#"
+              />
+					</PaginationItem>
+
+					{[...Array(pagesCount)].map((page, index) => 
+						<PaginationItem active={index === currentPage} key={index}>
+							<PaginationLink onClick={e => handlePagination(e, index)} href="#">
+								{index + 1}
+							</PaginationLink>
+						</PaginationItem>
+          )}
+
+					<PaginationItem disabled={currentPage >= pagesCount - 1}>              
+						<PaginationLink
+							onClick={e => handlePagination(e, currentPage + 1)}
+							next
+							href="#"
+						/>              
+					</PaginationItem>
+				</Pagination>
 			</div>    
     </div>
   );
